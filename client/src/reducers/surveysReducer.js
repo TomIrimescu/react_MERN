708ad9967e802a654af575e1768a88fad7011550
TomIrimescu <tom@irimescu.com>
2018-03-27 17:54:17 -0400

    heroku 3-27-2018

---
+++ client/src/reducers/surveysReducer.js
@@ -0,0 +1,16 @@
+import { FETCH_SURVEYS } from "../actions/types";
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_SURVEYS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+
+
+
+
+
