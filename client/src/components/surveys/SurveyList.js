2171f2d499b84c96f05371260865a3d0cd8f5a90
TomIrimescu <tom@irimescu.com>
2018-03-27 18:05:16 -0400

    heroku prettified file

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,16 +6,14 @@ class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
-  
+
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
       return (
         <div className="card darken-1" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{survey.title}</span>
-            <p>
-              {survey.body}
-            </p>
+            <p>{survey.body}</p>
             <p className="right">
               Sent On: {new Date(survey.dateSent).toLocaleDateString()}
             </p>
@@ -28,13 +26,9 @@ class SurveyList extends Component {
       );
     });
   }
-  
+
   render() {
-    return (
-      <div>
-        {this.renderSurveys()}
-      </div>
-      )
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
